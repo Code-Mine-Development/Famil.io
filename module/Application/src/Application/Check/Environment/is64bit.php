2cba28f31c253720d906818e3ed8cc7cd68a2664
Adam Grabek <adam@grabek.io>
2015-06-17 13:40:37 +0200

    Diagnostics moved to Module class

---
+++ module/Application/src/Application/Check/Environment/is64bit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adamgrabek
+ * Date: 17/06/15
+ * Time: 13:11
+ */
+
+namespace Application\Check\Environment;
+
+
+class is64bit
+{
+
+}
---
+++ module/Application/src/Application/Check/Environment/isUnix.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adamgrabek
+ * Date: 17/06/15
+ * Time: 13:12
+ */
+
+namespace Application\Check\Environment;
+
+
+class isUnix
+{
+
+}

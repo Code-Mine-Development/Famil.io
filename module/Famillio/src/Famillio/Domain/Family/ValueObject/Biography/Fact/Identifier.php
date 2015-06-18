00368703f3f221208ebba14be5b116d3dab38647
Adam Grabek <adam@grabek.io>
2015-06-18 10:43:14 +0200

    Identifier implementation - validation and date change factory (#3)

--- module/Famillio/src/Famillio/Domain/Family/ValueObject/Biography/Fact/Identifier.php
+++ module/Famillio/src/Famillio/Domain/Family/ValueObject/Biography/Fact/Identifier.php
@@ -11,8 +11,10 @@ namespace Famillio\Domain\Family\ValueObject\Biography\Fact;
 
 use AGmakonts\STL\AbstractValueObject;
 use AGmakonts\STL\DateTime\DateTime;
+use AGmakonts\STL\Number\Integer;
 use AGmakonts\STL\String\Text;
 use Rhumsaa\Uuid\Uuid;
+use Zend\Paginator\Adapter\ArrayAdapter;
 
 /**
  * Class Identifier
@@ -21,36 +23,52 @@ use Rhumsaa\Uuid\Uuid;
  */
 class Identifier extends AbstractValueObject
 {
-    const FORMAT = '%s@%\'.012d';
+    const DELIMITER = '@';
+    const FORMAT    = '%s' . self::DELIMITER . '%\'.012d';
 
-
+    /**
+     * @var \AGmakonts\STL\String\Text
+     */
     private $identifier;
 
+    /**
+     * @var \AGmakonts\STL\DateTime\DateTime
+     */
     private $date;
 
-
+    /**
+     * @param \AGmakonts\STL\String\Text $identifier
+     *
+     * @return \Famillio\Domain\Family\ValueObject\Biography\Fact\Identifier
+     */
     static public function get(Text $identifier) : Identifier
     {
-
+        return self::getInstanceForValue([$identifier]);
     }
 
     /**
      * @param \AGmakonts\STL\DateTime\DateTime $dateTime
+     *
+     * @return \Famillio\Domain\Family\ValueObject\Biography\Fact\Identifier
      */
     static public function generate(DateTime $dateTime) : Identifier
     {
-        $timestamp = $dateTime->getTimestamp()->value();
+        $textObject = self::formatIdentifier(Uuid::uuid4(), $dateTime);
 
-        $uuid = Uuid::uuid4();
-
-        return self::get(Text::get(sprintf(self::FORMAT, $uuid, $timestamp)));
+        return self::get($textObject);
     }
 
+    /**
+     * @return \AGmakonts\STL\String\Text
+     */
     public function identifier() : Text
     {
-
+        return $this->identifier;
     }
 
+    /**
+     * @return \AGmakonts\STL\DateTime\DateTime
+     */
     public function date() : DateTime
     {
         return $this->date;
@@ -58,10 +76,16 @@ class Identifier extends AbstractValueObject
 
     /**
      * @param \AGmakonts\STL\DateTime\DateTime $dateTime
+     *
+     * @return \Famillio\Domain\Family\ValueObject\Biography\Fact\Identifier
      */
     public function atChangedDate(DateTime $dateTime) : Identifier
     {
+        $valueParts    = $this->extractParts($this->identifier());
+        $uuid          = Uuid::fromString($valueParts['uuid']);
+        $newIdentifier = self::formatIdentifier($uuid, $dateTime);
 
+        return self::get($newIdentifier);
     }
 
     /**
@@ -70,7 +94,54 @@ class Identifier extends AbstractValueObject
      */
     protected function __construct(array $value)
     {
+        $value = $value[0];
 
+        $valueParts = $this->extractParts($value);
+
+        $dateTime = DateTime::get(Integer::get($valueParts['timestamp']));
+
+        if (FALSE === Uuid::isValid($valueParts['uuid'])) {
+
+        }
+
+        $this->identifier = $value;
+        $this->date       = $dateTime;
+
+    }
+
+    /**
+     * @param \AGmakonts\STL\String\Text $identifier
+     *
+     * @return array
+     */
+    private function extractParts(Text $identifier) : array
+    {
+        $value = $identifier->value();
+
+        $valueParts = explode(self::DELIMITER, $value);
+
+        $timestamp  = (int)$valueParts[0];
+        $scalarUuid = $valueParts[1];
+
+        return [
+            'timestamp' => $timestamp,
+            'uuid'      => $scalarUuid,
+        ];
+    }
+
+    /**
+     * @param \Rhumsaa\Uuid\Uuid               $uuid
+     * @param \AGmakonts\STL\DateTime\DateTime $dateTime
+     *
+     * @return \AGmakonts\STL\String\Text
+     */
+    static private function formatIdentifier(Uuid $uuid, DateTime $dateTime) : Text
+    {
+        $timestamp  = $dateTime->getTimestamp()->value();
+        $formatted  = sprintf(self::FORMAT, $uuid->toString(), $timestamp);
+        $textObject = Text::get($formatted);
+
+        return $textObject;
     }
 
     /**

6b049b34033e75a31e6a4f9ede3747b5df55a91f
Adam Grabek <adam@grabek.io>
2015-06-20 12:44:43 +0200

    Specifications refactored to use SpecificationAggregate instead of chaining individual specs

--- module/Famillio/src/Famillio/Domain/Family/Collection/Biography/Filter/SpecificationInterface.php
+++ module/Famillio/src/Famillio/Domain/Family/Collection/Biography/Filter/SpecificationInterface.php
@@ -25,29 +25,4 @@ interface SpecificationInterface
      * @return bool
      */
     public function isFactAcceptable(FactInterface $factInterface) : bool;
-
-    /**
-     * Attach another specification to the chain. All attached specifications will be
-     * used and Fact will be accepted only if all of them are satisfied.
-     *
-     * @param \Famillio\Domain\Family\Collection\Biography\Filter\SpecificationInterface $specificationInterface
-     *
-     * @return void
-     */
-    public function attach(SpecificationInterface $specificationInterface);
-
-    /**
-     * Determine if specification can be attached to another. Specification provided in argument will
-     * be a specification to witch current specification is being attached.
-     *
-     * @param \Famillio\Domain\Family\Collection\Biography\Filter\SpecificationInterface $specificationInterface
-     *
-     * @return bool
-     */
-    public function canBeAttachedTo(SpecificationInterface $specificationInterface) : bool;
-
-    /**
-     * @return \Famillio\Domain\Family\Collection\Biography\Filter\SpecificationInterface
-     */
-    public function child() : SpecificationInterface;
 }
